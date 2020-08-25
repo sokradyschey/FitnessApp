57068107f7f5e312a1017ec9601af07aa711d0d8
Sokrady Chey <sokradychey@gmail.com>
2020-08-25 16:03:05 -0400

    Countdown Timer Addition

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -19,14 +19,14 @@ export default function BottomTabNavigator() {
       initialRouteName="TabOne"
       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
       <BottomTab.Screen
-        name="TabOne"
+        name="Home"
         component={TabOneNavigator}
         options={{
           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
+        name="Timer"
         component={TabTwoNavigator}
         options={{
           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
@@ -52,7 +52,7 @@ function TabOneNavigator() {
       <TabOneStack.Screen
         name="TabOneScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+        options={{ headerTitle: 'Home' }}
       />
     </TabOneStack.Navigator>
   );
@@ -66,7 +66,7 @@ function TabTwoNavigator() {
       <TabTwoStack.Screen
         name="TabTwoScreen"
         component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
+        options={{ headerTitle: 'Timer' }}
       />
     </TabTwoStack.Navigator>
   );
---
+++ timer/CountdownTimer.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { View, StyleSheet, Animated, Button } from 'react-native';
+import Constants from 'expo-constants';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+
+export default function Timer({ path }: { path: string }) {
+  return (
+    <View style={styles.container}>
+      <CountdownCircleTimer
+        isPlaying
+        duration={10}
+        onComplete={() => alert('finished')}
+        colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}>
+        {({ remainingTime, animatedColor }) => (
+          <Animated.Text
+            style={{ ...styles.remainingTime, color: animatedColor }}>
+            {remainingTime}
+          </Animated.Text>
+        )}
+      </CountdownCircleTimer>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: .50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // paddingTop: Constants.statusBarHeight,
+    padding: 20,
+  },
+  remainingTime: {
+    fontSize: 46,
+  },
+});
