8324e9bbaf2e145a6378f56e1ea4679805f0545e
Sokrady Chey <sokradychey@gmail.com>
2020-08-27 23:19:12 -0400

    removed unneeded css

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -19,35 +19,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     alignItems: "center",
   },
-  modalView: {
-    justifyContent: "space-around",
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 20,
-    padding:20,
-    elevation: 2
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    textAlign: "center"
-  }
 });
 
 export default workoutOptions;
